4aef8cfc72ac0a0c064e2a0829de49dd68ddf97e
Manu Kumar Pal <102711650+manukumar07@users.noreply.github.com>
2025-08-18 18:27:55 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -22,7 +22,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/users" element={<Users />} />
-          <Route path="/*" element={<NotFound />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </Suspense>
 
